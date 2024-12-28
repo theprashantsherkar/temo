e2e4f5ea186fc3b577a981abbcb56d6ac2a9bb50
Prashant <prashantsherkar7515400@gmail.com>
2024-12-28 18:25:56 +0530

    fixing foter

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,20 +6,20 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 export default function Footer() {
     return (
-        <footer className="flex  flex-col  items-center justify-between  bg-gray-800 text-white text-center">
-            <div className='flex items-start justify-between p-7 bg-gray-800 text-white text-center'>
-                <div className='w-1/4 flex items-center justify-start flex-col gap-3'>
+        <footer className="flex flex-col  border  border-white  items-center justify-between  bg-gray-800 text-white text-center">
+            <div className='flex sm:flex-row flex-col items-start justify-between gap-4 p-7 bg-gray-800 text-white text-center'>
+                <div className='w-full sm:w-1/4 flex items-center justify-start flex-col gap-3'>
                     <Link href={'https://djsceietesf.wordpress.com/'} target='blank'><h1 className='text-xl font-semibold'>DJS IETE-ISF</h1></Link>
                     <p>The IETE Students’ Forum of D. J. Sanghvi College of Engineering</p>
 
                 </div>
-                <div className='w-1/4 flex items-center justify-start flex-col gap-3'>
+                <div className='w-full  sm:w-1/4 flex items-center justify-start flex-col gap-3'>
                     <h1 className='text-xl font-semibold'>Address</h1>
                     <p>Dwarkadas J. Sanghvi College of Engineering,
                         Vile Parle West, Mumbai-400056</p>
 
                 </div>
-                <div className='min-w-1/4 flex items-center justify-start flex-col gap-3'>
+                <div className='w-full  sm:w-1/4 flex items-center justify-start flex-col gap-3'>
                     <h1 className='text-xl font-semibold'>Contact</h1>
                     <div>
                         <p>Utkarsh Lotiya: +91 93220 34012</p>
@@ -27,7 +27,7 @@ export default function Footer() {
                         <p>Samay Mehta: +91 79772 37204</p>
                     </div>
                 </div>
-                <div className='w-fit flex items-center justify-start flex-col gap-4'>
+                <div className='w-full sm:w-fit flex items-center justify-start flex-col gap-4'>
                     <h1 className='text-xl font-semibold'>Follow us on</h1>
                     <div className="flex items-center justify-center gap-3">
                         <Link href="https://www.instagram.com/djsce_iete/" className='p-3 hover:scale-110 border-2 border-white rounded-full hover:bg-slate-900 transition-all' target="_blank" rel
@@ -38,10 +38,10 @@ export default function Footer() {
                             ="noopener noreferrer" className='p-3 hover:scale-110 border-2 border-white rounded-full hover:bg-slate-900 transition-all'>
                             <LinkedInIcon />
                         </Link>
-                        <Link href="https://www.linkedin.com/company/djsce-iete/posts/?feedView=all" target="_blank" rel
+                        <a href="mailto:djsceiete2024@gmail.com" target="_blank" rel
                             ="noopener noreferrer" className='p-3 border-2 hover:scale-110 border-white rounded-full hover:bg-slate-900 transition-all'>
                             <MailOutlineIcon />
-                        </Link>
+                        </a>
                     </div>
                 </div>
             </div>
