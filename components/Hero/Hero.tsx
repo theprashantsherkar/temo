dd5e6fc4ee4bcff3f17320a3644b8d6400511636
Prashant <prashantsherkar7515400@gmail.com>
2025-01-08 16:57:25 +0530

    image issue foxed

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,12 +1,13 @@
 import RegisterBtn from '../RegsterBtn/RegisterBtn'
 import './heroStyle.css'
 import BoxReveal from '@/components/ui/box-reveal'
+import Image from 'next/image'
 
 export default function Hero() {
     return (
         <div className="flex items-center justify-center flex-col gap-6 mx-9">
             <BoxReveal duration={0.5} boxColor='#5046e6'>
-                <img src="/unplugged.png" alt="logo" className="oscillatey scale-50 sm:scale-0 h-[150px]" />
+                <Image src="/unplugged.png" alt="logo" className="oscillatey scale-50 h-[150px]" width={750} height={150} />
             </BoxReveal>
             <BoxReveal duration={0.7} boxColor='#5046e6'>
 
@@ -14,7 +15,7 @@ export default function Hero() {
                 A 24 hour hardware hackathon for the tech enthusiasts all over the country.
             </div>
             </BoxReveal>
-            <BoxReveal duration={1.6} boxColor='#5046e6'>
+            <BoxReveal duration={0.9} boxColor='#5046e6'>
                 <RegisterBtn />
             </BoxReveal>
         </div>
