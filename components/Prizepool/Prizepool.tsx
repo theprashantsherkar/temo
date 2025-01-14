65daa35a13f20d0240414a0f698fd97e5a5c6282
Prashant <prashantsherkar7515400@gmail.com>
2025-01-15 01:34:12 +0530

    bg fixed

--- components/Prizepool/Prizepool.tsx
+++ components/Prizepool/Prizepool.tsx
@@ -1,31 +1,32 @@
 'use client';
 import Badge from "../custom/Badge";
 import React, { useEffect } from "react";
-import AOS from "aos";
+import { ImagesSlider } from "../ui/images-slider";
 import "aos/dist/aos.css";
 
 export default function Prizepool() {
-    useEffect(() => {
-        AOS.init({
-            duration: 2000,
-            delay: 100,
-        });
-    });
+    const images = [
+        "/threeCollage.jpg",
+        "/timelinebg2.jpg",
+        "/bridge.jpg",
+    ]
 
     return (
-        <div className="prizepool sm:h-screen h-min sm:py-4 py-5 text-white bg-black prizepoolBg flex gap-6 flex-col items-center justify-center " id="prizepool">
-            <h1 className="text-3xl text-white font-semibold oscillatey pt-7">Prizepool</h1>
-            <div>
-                <Badge label="1st Prize" image="/prize/first.png" />
-            </div>
-            <div className="flex sm:flex-row flex-col items-center justify-center gap-6 sm:gap-12">
+        <ImagesSlider images={images}>
+            <div className="prizepool  sm:h-screen h-min z-50  py-5 text-white   flex gap-6 flex-col items-center justify-center " id="prizepool">
+                <h1 className="text-3xl text-white font-semibold oscillatey pt-7 ">Prizepool</h1>
                 <div>
-                    <Badge label="2nd Prize" image="/prize/second.png" />
+                    <Badge label="1st Prize" image="/prize/first.png" />
                 </div>
-                <div>
-                    <Badge label="3nd Prize" image="/prize/third.png" />
+                <div className="flex sm:flex-row flex-col items-center justify-center gap-6 sm:gap-12">
+                    <div>
+                        <Badge label="2nd Prize" image="/prize/second.png" />
+                    </div>
+                    <div>
+                        <Badge label="3nd Prize" image="/prize/third.png" />
+                    </div>
                 </div>
             </div>
-        </div>
+        </ImagesSlider>
     );
 };
--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -5,7 +5,7 @@ import { HoverEffect } from '../ui/card-hover-effect'
 
 function Contact() {
     return (
-        <div className=' w-full py-10  flex flex-col items-center justify-center text-center bg-black mx-auto'>
+        <div className=' w-full py-10  flex flex-col items-center justify-center text-center bg-black mx-auto' id='contact'>
             <h1 className=' font-semibold text-white text-3xl'>Contact us at:</h1>
             <div className='w-3/4 mx-auto'>
                 <HoverEffect items={poc} />
