c7505be0147e6b80cbd15a1f471c512a2a54a3bc
Prashant <prashantsherkar7515400@gmail.com>
2025-01-16 22:47:34 +0530

    footer fixed

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,27 +7,27 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 export default function Footer() {
     return (
         <footer className="flex flex-col   items-center justify-between  bg-sky-900 text-white text-center">
-            <div className='flex sm:flex-row flex-col items-start justify-between gap-4 p-7 bg-sky-950 text-white text-center'>
-                <div className='w-full sm:w-1/4 flex items-center justify-start flex-col gap-3'>
+            <div className='flex sm:flex-row flex-col items-start justify-around gap-4  p-7 bg-sky-950 text-white text-center'>
+                <div className='w-full sm:w-1/3 flex items-center justify-start flex-col gap-3'>
                     <Link href={'https://djsceietesf.wordpress.com/'} target='blank'><h1 className='text-xl font-semibold'>DJS IETE-ISF</h1></Link>
                     <p>The IETE Students’ Forum of D. J. Sanghvi College of Engineering</p>
 
                 </div>
-                <div className='w-full  sm:w-1/4 flex items-center justify-start flex-col gap-3'>
+                <div className='w-full  sm:w-1/3 flex items-center justify-start flex-col gap-3'>
                     <h1 className='text-xl font-semibold'>Address</h1>
                     <p>Dwarkadas J. Sanghvi College of Engineering,
                         Vile Parle West, Mumbai-400056</p>
 
                 </div>
-                <div className='w-full  sm:w-1/4 flex items-center justify-start flex-col gap-3'>
+                {/* <div className='w-full  sm:w-1/4 flex items-center justify-start flex-col gap-3'>
                     <h1 className='text-xl font-semibold'>Contact</h1>
                     <div>
                         <p>Utkarsh Lotiya: +91 93220 34012</p>
                         <p>Aneesh Joshi: +91 70454 91990</p>
                         <p>Samay Mehta: +91 79772 37204</p>
                     </div>
-                </div>
-                <div className='w-full sm:w-fit flex items-center justify-start flex-col gap-4'>
+                </div> */}
+                <div className='w-full sm:w-1/3 flex items-center justify-start flex-col gap-4'>
                     <h1 className='text-xl font-semibold'>Follow us on</h1>
                     <div className="flex items-center justify-center gap-3">
                         <Link href="https://www.instagram.com/djsce_iete/" className='p-3 hover:scale-110 border-2 border-white rounded-full hover:bg-black  transition-all' target="_blank" rel
